a9c3e7307acd76cbaaa614395433814501edbd1a
Peter Alexander <makeroftools@creativedevelopment.systems>
2023-10-03 13:50:37 -0400

    started some notes, etc

---
+++ cbp/my_decorators.py
@@ -0,0 +1,12 @@
+import time
+
+def profile_callable(func):
+    def wrapper(*args, **kwargs):
+        start_time = time.time()
+        result = func(*args, **kwargs)
+        end_time = time.time()
+        elapsed_time = end_time - start_time
+        print(f"{func.__name__} took {elapsed_time:.6f} seconds to run")
+        return result 
+    return wrapper 
+
