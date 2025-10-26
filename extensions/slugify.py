e457d9a04c5ffbdf1d28372e7e7e3ff8671cb24e
Peter Alexander <makeroftools@creativedevelopment.systems>
2025-10-26 12:42:00 -0600

    working on base template

---
+++ extensions/slugify.py
@@ -0,0 +1,22 @@
+import re
+import unicodedata
+from jinja2.ext import Extension
+
+
+
+def slugify(value, allow_unicode=False):
+    """
+    """
+    value = str(value)
+    if allow_unicode:
+        value = unicodedata.normalize('NFKC', value)
+    else:
+        value = unicodedata.normalize('NFKD', value).encode('ascii', 'ignore').decode('ascii')
+    value = re.sub(r'[^\w\s-]', '', value.lower())
+    return re.sub(r'[-\s]+', '-', value).strip('-_')
+
+
+class SlugifyExtension(Extension):
+    def __init__(self, environment):
+        super().__init__(environment)
+        environment.filters["slugify"] = slugify 
