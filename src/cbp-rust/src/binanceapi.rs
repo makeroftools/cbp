c94d65878942e25c2ce3e25c1da270ac47b14efd
Peter Alexander <makeroftools@creativedevelopment.systems>
2025-11-24 16:20:33 -0700

    converting to graphql types.. is a pita

--- src/cbp-rust/src/binanceapi.rs
+++ src/cbp-rust/src/binanceapi.rs
@@ -2,14 +2,72 @@ use async_graphql;
 use async_graphql::*;
 use binance_sdk::spot::rest_api::RestApi;
 use binance_sdk::spot::rest_api::ExchangeInfoParams;
-use binance_sdk::spot::rest_api::ExchangeInfoResponse;
+// use binance_sdk::spot::rest_api::ExchangeInfoResponse;
 use std::sync::Arc;
 // use serde_json;
 use tokio::sync::Mutex;
- 
+  
 #[derive(SimpleObject)]
-#[graphql(name = "ExchangeInfoResponse")]
-struct ExchangeInfo(ExchangeInfoResponse);
+struct RateLimit {
+    pub interval: String,
+    pub limit: i64,
+    pub interval_num: i64,
+    pub rate_limit_type: String,
+}
+
+#[derive(SimpleObject)]
+pub struct Filters {
+    pub filter_type: String,
+    pub max_price: String,
+    pub min_price: String,
+    pub tick_size: String,
+    pub max_qty: String,
+    pub min_qty: String,
+    pub step_size: String,
+    pub limit: i64,
+    pub multiplier_up: String,
+    pub multiplier_down: String,
+    pub multiplier_decimal: String,
+}
+
+
+#[derive(SimpleObject)]
+pub struct Symbols {
+    pub filters: Vec<Filters>,
+    pub order_type: Vec<String>,
+    pub time_in_force: Vec<String>,
+    pub liquidation_fee: String,
+    pub market_take_bound: String,
+    pub symbol: String,
+    pub pair: String,
+    pub contract_type: String,
+    pub delivery_date: i64,
+    pub onboard_date: i64,
+    pub contract_status: String,
+    pub contract_size: i64,
+    pub quote_asset: String,
+    pub base_asset: String,
+    pub margin_asset: String,
+    pub price_precision: i64,
+    pub quantity_precision: i64,
+    pub base_asset_precision: i64,
+    pub quote_precision: i64,
+    pub equal_qty_precision: i64,
+    pub trigger_protect: String,
+    pub maint_margin_percent: String,
+    pub required_margin_percent: String,
+    pub underlying_type: String,
+    pub underlying_sub_type: Vec<String>,
+}
+
+#[derive(SimpleObject)]
+struct ExchangeInfo {
+    exchange_filters: Vec<String>,
+    rate_limits: Vec<RateLimit>,
+    server_time: i64,
+    symbols: Vec<String>,
+    timezone: String
+}
 
 pub struct BinanceQuery;
 
@@ -42,7 +100,14 @@ impl BinanceQuery {
             .await?
             .data()
             .await?;
-        Ok(ExchangeInfo)
+        let ret = ExchangeInfo {
+            exchange_filters: data.exchange_filters,
+            rate_limits: data.rate_limits,
+            server_time: data.server_time.unwrap(),
+            symbols: data.symbols,
+            timezone: data.timezone,
+        };
+        Ok(ret)
     }
 }
 
