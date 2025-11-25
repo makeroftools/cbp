ed746fd527189ebb26ff4ed52ce00f3375610be1
Peter Alexander <makeroftools@creativedevelopment.systems>
2025-11-25 14:14:39 -0700

    auto-generated async-graphql models

--- src/cbp-rust/src/binance/binanceapi.rs
+++ src/cbp-rust/src/binance/binanceapi.rs
@@ -7,67 +7,7 @@ use std::sync::Arc;
 // use serde_json;
 use tokio::sync::Mutex;
   
-#[derive(SimpleObject)]
-struct RateLimit {
-    pub interval: String,
-    pub limit: i64,
-    pub interval_num: i64,
-    pub rate_limit_type: String,
-}
 
-#[derive(SimpleObject)]
-pub struct Filters {
-    pub filter_type: String,
-    pub max_price: String,
-    pub min_price: String,
-    pub tick_size: String,
-    pub max_qty: String,
-    pub min_qty: String,
-    pub step_size: String,
-    pub limit: i64,
-    pub multiplier_up: String,
-    pub multiplier_down: String,
-    pub multiplier_decimal: String,
-}
-
-
-#[derive(SimpleObject)]
-pub struct Symbols {
-    pub filters: Vec<Filters>,
-    pub order_type: Vec<String>,
-    pub time_in_force: Vec<String>,
-    pub liquidation_fee: String,
-    pub market_take_bound: String,
-    pub symbol: String,
-    pub pair: String,
-    pub contract_type: String,
-    pub delivery_date: i64,
-    pub onboard_date: i64,
-    pub contract_status: String,
-    pub contract_size: i64,
-    pub quote_asset: String,
-    pub base_asset: String,
-    pub margin_asset: String,
-    pub price_precision: i64,
-    pub quantity_precision: i64,
-    pub base_asset_precision: i64,
-    pub quote_precision: i64,
-    pub equal_qty_precision: i64,
-    pub trigger_protect: String,
-    pub maint_margin_percent: String,
-    pub required_margin_percent: String,
-    pub underlying_type: String,
-    pub underlying_sub_type: Vec<String>,
-}
-
-#[derive(SimpleObject)]
-struct ExchangeInfo {
-    exchange_filters: Vec<String>,
-    rate_limits: Vec<RateLimit>,
-    server_time: i64,
-    symbols: Vec<String>,
-    timezone: String
-}
 
 pub struct BinanceQuery;
 
